58355a9003b779bd3b7114f60dfd6c81eb02b872
Maxi! <mxportocarrero@gmail.com>
2020-02-02 01:57:05 -0500

    [UPDATE] added Keypress events

--- client/js/game.js
+++ client/js/game.js
@@ -10,3 +10,42 @@ socket.on("server:playersNewPositions", data => {
     ctx.fillText(player.number, player.x, player.y)
   })
 })
+
+document.onkeydown = event =>{
+  console.log(event.keyCode)
+  if(event.keyCode === 68)
+    socket.emit('keypress', {
+      inputID:'right', state: true
+    })
+  else if(event.keyCode === 83)
+    socket.emit('keypress', {
+      inputID:'down', state: true
+    })
+  else if(event.keyCode === 65)
+    socket.emit('keypress', {
+      inputID:'left', state: true
+    })
+  else if(event.keyCode === 87)
+    socket.emit('keypress', {
+      inputID:'up', state: true
+    })
+}
+
+document.onkeyup = event =>{
+  if(event.keyCode === 68)
+    socket.emit('keypress', {
+      inputID:'right', state: false
+    })
+  else if(event.keyCode === 83)
+    socket.emit('keypress', {
+      inputID:'down', state: false
+    })
+  else if(event.keyCode === 65)
+    socket.emit('keypress', {
+      inputID:'left', state: false
+    })
+  else if(event.keyCode === 87)
+    socket.emit('keypress', {
+      inputID:'up', state: false
+    })
+}
--- index.js
+++ index.js
@@ -21,37 +21,72 @@ console.log("Server started")
 
 
 // sockets
-const socketList = []
+const socketList = {} // must be an object because it behaves like a hash
+const playerList = {}
 const io = SocketIO(server)
+
+const Player = (id) => {
+  const self = {
+    x:250,
+    y:250,
+    id,
+    number: "" + Math.floor(10 * Math.random()),
+    pressingRight: false,
+    pressingLeft: false,
+    pressingUp: false,
+    pressingDown: false,
+    maxSpeed: 10,
+  }
+
+  self.updatePosition = () => {
+    if(self.pressingRight)
+      self.x += self.maxSpeed
+    if(self.pressingLeft)
+      self.x -= self.maxSpeed
+    if(self.pressingUp)
+      self.y -= self.maxSpeed
+    if(self.pressingDown)
+      self.y += self.maxSpeed
+
+  }
+  return self
+}
 
 io.sockets.on('connection', (socket => {
   // socket configuration
   socket.id = parseInt((Math.random()*1000000), 10);
-  socket.x = 0;
-  socket.y = 0;
-  socket.number = "" + Math.floor(10 * Math.random())
   console.log("socket connection with id", socket.id)
+  const player = Player(socket.id)
+  playerList[socket.id] = player
   socketList[socket.id] = socket
 
   socket.on('disconnect', () => {
     delete socketList[socket.id]
+    delete playerList[socket.id]
   })
+
+  socket.on('keypress', data => {
+    if(data.inputID === 'left')
+      player.pressingLeft = data.state
+    else if(data.inputID === 'right')
+      player.pressingRight = data.state
+    else if(data.inputID === 'up')
+      player.pressingUp = data.state
+    else if(data.inputID === 'down')
+      player.pressingDown = data.state
+    })
 
 }));
 
 setInterval(() => {
-  const playerPositions = []
-  socketList.forEach(socket => {
-    socket.x++;
-    socket.y++;
-    playerPositions.push({
-      x:socket.x,
-      y:socket.y,
-      number: socket.number
-    })
+  const playerPositions = Object.keys(playerList).map(key =>{
+    const player = playerList[key]
+    player.updatePosition()
+    return player
   })
 
-  socketList.forEach(socket => {
+  Object.keys(socketList).forEach(key => {
+    const socket = socketList[key]
     socket.emit('server:playersNewPositions', playerPositions);
   })
-}, 1000/20);
+}, 1000/25);
